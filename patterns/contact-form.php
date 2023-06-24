557a3a822a6e7828779e94f1c365f35d1e592b17
Orzo Cogorzo <orzocogorzo@hotmail.com>
2023-06-24 18:24:03 +0200

    feat: add contact form pattern

---
+++ patterns/contact-form.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: Contact Form
+ * Slug: greenurban/contact-form
+ * Categories: form
+ * Keywords: contact-form
+ * Block Types: core/template-part/contact-form
+ */
+?>
+
+<form id="contactForm" method="POST">
+	<label for="name">Tu nombre</label>
+	<input type="text" name="name" autocomplete="name" />
+	<label for="email">Correo electrònico</label>
+	<input type="email" name="email" autocomplete="email" />
+	<label for="message">Mensaje</label>
+	<textarea name="message"></textarea>
+</form>
+<!-- /wp:group -->
